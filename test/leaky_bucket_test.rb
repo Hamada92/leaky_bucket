74949f24467e115b3902fa69c8052c0aaf12dc60
Ahmad <ahmad.kaate@gmail.com>
2017-10-07 10:31:47 -0400

    test configs

--- test/leaky_bucket_test.rb
+++ test/leaky_bucket_test.rb
@@ -2,14 +2,18 @@ require "test_helper"
 
 class LeakyBucketTest < Minitest::Test
 
+  def setup
+    LeakyBucket.cache = ActiveSupport::Cache::MemoryStore.new
+  end
+
    def test_it_creates_new_cache_entry_on_the_first_request
     #cache doesn't exist
-    assert_not Rails.cache.read(1)
+    refute LeakyBucket.cache.read(1)
 
     LeakyBucket::Throttler.throttle(1)
 
     #cache exists
-    assert Rails.cache.read(1)
+    assert LeakyBucket.cache.read(1)
   end
 
   def test_that_it_has_a_version_number
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,5 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "leaky_bucket"
-
+require 'active_support/cache'
+require 'active_support/cache/memory_store'
 require "minitest/autorun"
